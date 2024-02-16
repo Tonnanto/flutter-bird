be73deb2d82381008ff9ca43842e0b45e0497a19
Anton Stamme <anton@stamme.de>
2024-02-16 20:00:29 +0100

    added dependencies and integrated web3modal (WIP)

--- flutter_bird_app/lib/config.dart
+++ flutter_bird_app/lib/config.dart
@@ -1,4 +1,4 @@
-const chainId = 5; // Goerli Testnet
+const chainId = 'eip155:5'; // Goerli Testnet
 const flutterBirdSkinsContractAddress = '0x387f544E4c3B2351d015dF57c30831Ad58D6C798';
 const ipfsGatewayUrl = 'https://nftstorage.link/ipfs/';
 const alchemyNodeProviderUrl = 'https://eth-goerli.g.alchemy.com/v2/';
